b94c6051749804ff6fcd21f7c2717e798c39fadc
Narawit Sukwattanajaroon <narawichsukwattanajaroon@gmail.com>
2022-11-25 18:19:22 +0700

    feat: factory user unique email

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,7 @@
 
 FactoryBot.define do
     factory :user do
-      email { 'test@gmail.com' }
+      sequence(:email) { |n| "user#{n}@email.com" }
       password { 'thisistestpassword' }
     end
   end
